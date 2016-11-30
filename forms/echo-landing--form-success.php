f47a01202ceca7c9325444365fd325b109140cc0
Joseph ODriscoll <joey@jodriscoll.com>
2016-11-30 18:03:20 -0500

    pushing up the current state to sync up with my partner in crime

---
+++ forms/echo-landing--form-success.php
@@ -0,0 +1,48 @@
+<?php
+  if( $_POST ) {
+/*  $variable       = $_POST['name definition from the form'] */
+    $first_name     = $_POST['first_name'];
+    $last_name      = $_POST['last_name'];
+    $business_email = $_POST['email'];
+    $phone_number   = $_POST['phone'];
+    $company_name   = $_POST['company']; 
+    $job_title      = $_POST['title'];
+
+    ?>
+    
+    <table class="table table-striped" border="0">
+      <tr>
+        <td colspan="2">
+         <div class="alert alert-info">
+          <strong>Success</strong>, Form Submitted Successfully...
+         </div>
+        </td>
+      </tr>
+      <tr>
+        <td>First Name</td>
+        <td><?php echo $first_name ?></td>
+      </tr>
+      <tr>
+        <td>Last Name</td>
+        <td><?php echo $last_name ?></td>
+      </tr>
+      <tr>
+        <td>Business Email</td>
+        <td><?php echo $business_email; ?></td>
+      </tr>
+      <tr>
+        <td>Phone Number</td>
+        <td><?php echo $phone_number; ?></td>
+      </tr>
+      <tr>
+        <td>Company Name</td>
+        <td><?php echo $company_name; ?></td>
+      </tr>
+      <tr>
+        <td>Job Title</td>
+        <td><?php echo $job_title; ?></td>
+      </tr>
+    </table>
+
+<?php
+  }
